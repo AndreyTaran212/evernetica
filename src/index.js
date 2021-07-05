4deea57f66d7973e243f188d603700353953a1f8
andrey <tarangad313@gmail.com>
2021-07-05 12:50:55 +0300

    update index.js and App.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import { createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { rootReducer } from './redux/rootReducer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import history from "./history";
 
-const store = createStore(rootReducer, compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, compose(applyMiddleware(
+    thunk
+), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
 const app = (
   <Provider store={store}>
     <App/>
   </Provider>
 )
 
-render(app, document.getElementById('root'));
+render( <Router history={history}>{app}</Router>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
