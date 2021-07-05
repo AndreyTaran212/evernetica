998514b3fe59abd482f78f85264f394754e32edb
andrey <tarangad313@gmail.com>
2021-07-05 12:48:01 +0300

    add card info page

---
+++ src/pages/CardInfoPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+import {useSelector} from 'react-redux';
+
+function CardInfoPage() {
+    const countries = useSelector(state => state.card.country)
+    const {code} = useParams();
+    const oneCountry = countries.find(item => item.name === code.slice(1)) || JSON.parse(localStorage.getItem("onecountry"))
+    localStorage.setItem("onecountry", JSON.stringify(oneCountry));
+
+
+    return (
+        <div>
+            <h1>{oneCountry.name}</h1>
+            <p>Alt spelling:</p>
+            {oneCountry.altSpellings.length ? oneCountry.altSpellings.map((spelling, index) => {
+                return <li key={index}>{spelling}</li>
+            }) : <p>none</p>}
+            <p>Borders:</p>
+            {oneCountry.borders.length ? oneCountry.borders.map((border, index) => {
+                return <li key={index}>{border}</li>
+            }) : <p>none</p>}
+        </div>
+    )
+}
+
+export default CardInfoPage
