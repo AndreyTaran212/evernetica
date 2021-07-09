a013ac1276458c24726f2779ec835605685e8cbe
andrey <tarangad313@gmail.com>
2021-07-10 01:40:51 +0300

    change reducer

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,48 +1,57 @@
-import {FETCH_COUNTRY, HIDE_LOADER, SHOW_LOADER, DELETE_COUNTRY, ADD_TO_FAVORITE, REMOVE_FAVORITE} from "./types";
+import {
+    FETCH_COUNTRY,
+    HIDE_LOADER,
+    SHOW_LOADER,
+    DELETE_COUNTRY,
+    ADD_TO_FAVORITE,
+    REMOVE_FAVORITE
+} from './types';
 
-export function  fetchCountry(name){
-    if (name.length > 0){
+export function fetchCountry(name, status) {
+    if (name.length > 0) {
         return async dispatch => {
             dispatch(showLoader())
             const response = await fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-            const json =  await response.json()
+            const json = await response.json()
+            json.map((country, index) => country.isFavorite = status)
             dispatch({type: FETCH_COUNTRY, payload: json})
             dispatch(hideLoader())
         }
 
     }
-    return dispatch => {dispatch({type: FETCH_COUNTRY, payload: []})}
+    return dispatch => {
+        dispatch({type: FETCH_COUNTRY, payload: []})
+    }
 }
 
-export function  showLoader() {
+export function showLoader() {
     return {
         type: SHOW_LOADER
     }
 }
 
-export function  hideLoader() {
+export function hideLoader() {
     return {
         type: HIDE_LOADER
     }
 }
 
-export function deleteCountry(index){
+export function deleteCountry(index) {
     return {
         type: DELETE_COUNTRY,
         payload: index
     }
 }
 
-export function addToFavorite(data){
+export function addToFavorite(data) {
     return {
         type: ADD_TO_FAVORITE,
-        payload: [data]
+        payload: data
     }
-
 }
 
-export function removeFavorite(index){
-    return{
+export function removeFavorite(index) {
+    return {
         type: REMOVE_FAVORITE,
         payload: index
     }
--- src/redux/cardReducer.js
+++ src/redux/cardReducer.js
@@ -1,4 +1,4 @@
-import {ADD_TO_FAVORITE, DELETE_COUNTRY, FETCH_COUNTRY, REMOVE_FAVORITE} from "./types";
+import {ADD_TO_FAVORITE, DELETE_COUNTRY, FETCH_COUNTRY, REMOVE_FAVORITE} from './types';
 
 const initialState = {
     country: [],
@@ -8,13 +8,16 @@ const initialState = {
 export const countryReducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_COUNTRY:
-            return {...state, country: action.payload}
+            return {...state, country: state.country = action.payload}
         case DELETE_COUNTRY:
             return {...state, country: state.country.filter((country, index) => index !== action.payload)}
         case ADD_TO_FAVORITE:
             return {...state, favoriteCountry: state.favoriteCountry.concat(action.payload)}
         case REMOVE_FAVORITE:
-            return {...state, favoriteCountry: state.favoriteCountry.filter((country, index) => index !== action.payload)}
+            return {
+                ...state, country: state.country.concat([state.favoriteCountry[action.payload]]),
+                favoriteCountry: state.favoriteCountry.filter((country, index) => index !== action.payload)
+            }
         default:
             return state
     }
